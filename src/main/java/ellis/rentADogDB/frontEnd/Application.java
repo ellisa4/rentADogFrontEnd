f039444295e6f41cd0dadfe725805cf36850a860
ellis <ellisa4@xavier.edu>
2018-10-31 11:33:44 -0400

    Ellis | Spring | added skeleton

---
+++ src/main/java/ellis/rentADogDB/frontEnd/Application.java
@@ -0,0 +1,16 @@
+package ellis.rentADogDB.frontEnd;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Throwable
+    {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
