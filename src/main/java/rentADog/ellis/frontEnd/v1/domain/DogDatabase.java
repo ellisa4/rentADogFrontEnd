729a801403345f3550ccd506ac304e12cc2380b1
ellisa4 <ellisa4@xavier.edu>
2018-11-27 23:35:25 -0500

    Ellis | Java | added navigation

--- src/main/java/rentADog/ellis/frontEnd/v1/domain/DogDatabase.java
+++ src/main/java/rentADog/ellis/frontEnd/v1/domain/DogDatabase.java
@@ -55,7 +55,7 @@ public class DogDatabase {
                 + "'" +breed + "', "
                 + "'" +name + "',"
                 + "'" +image + "', "
-                + available + ");";
+                + available + ", '' );";
 
         try
         {
